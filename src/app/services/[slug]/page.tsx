eb296f26c9a44e7f4d82714df68ca0c0e6065cd9
VilutSilviu <vilut.chisalita@smartboxdigital.com>
2025-07-24 11:35:36 +0200

    Update /services/[slug]

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -1,12 +1,38 @@
 import { prisma } from '@/lib/prisma'
 import { notFound } from 'next/navigation'
 import ServiceDetail from '@/components/ServiceDetail'
+import { Metadata } from 'next'
 
-export default async function ServicePage({
-  params,
-}: {
-  params: { slug: string }
-}) {
+type Props = {
+  params: {
+    slug: string
+  }
+}
+
+export async function generateStaticParams() {
+  const services = await prisma.service.findMany({
+    select: { slug: true },
+  })
+
+  return services.map(service => ({
+    slug: service.slug,
+  }))
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const service = await prisma.service.findUnique({
+    where: { slug: params.slug },
+  })
+
+  if (!service) return {}
+
+  return {
+    title: service.title,
+    description: service.descriptionLong,
+  }
+}
+
+export default async function ServicePage({ params }: Props) {
   const service = await prisma.service.findUnique({
     where: { slug: params.slug },
   })
